635592f8c51ff16d7bd367008dbe2967318a4791
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2022-09-11 10:18:28 +0300

    devnet, testnet, mainnet deeplinks tests (#3) (#4)

--- VCL/src/test/java/io/velocitycareerlabs/entities/VCLDeepLinkTest.kt
+++ VCL/src/test/java/io/velocitycareerlabs/entities/VCLDeepLinkTest.kt
@@ -19,23 +19,67 @@ internal class VCLDeepLinkTest {
     }
 
     @Test
-    fun testPresentationRequestDeepLinkValidAggregation() {
+    fun testPresentationRequestDeepLinkDevNetValidAggregation() {
 
-        subject = VCLDeepLink(value = DeepLinkMocks.PresentationRequestDeepLinkStr)
+        subject = VCLDeepLink(value = DeepLinkMocks.PresentationRequestDeepLinkDevNetStr)
 
-        assert(subject.value == DeepLinkMocks.PresentationRequestDeepLinkStr)
-        assert(subject.value.decode() == DeepLinkMocks.PresentationRequestDeepLinkStr.decode())
+        assert(subject.value == DeepLinkMocks.PresentationRequestDeepLinkDevNetStr)
+        assert(subject.value.decode() == DeepLinkMocks.PresentationRequestDeepLinkDevNetStr.decode())
         assert(subject.requestUri == DeepLinkMocks.PresentationRequestRequestDecodedUriStr)
         assert(subject.vendorOriginContext == DeepLinkMocks.PresentationRequestVendorOriginContext)
     }
 
     @Test
-    fun testCredentialManifestDeepLinkValidAggregation() {
+    fun testPresentationRequestDeepLinkTestNetValidAggregation() {
 
-        subject = VCLDeepLink(value = DeepLinkMocks.CredentialManifestDeepLinkStr)
+        subject = VCLDeepLink(value = DeepLinkMocks.PresentationRequestDeepLinkTestNetStr)
 
-        assert(subject.value == DeepLinkMocks.CredentialManifestDeepLinkStr)
-        assert(subject.value.decode() == DeepLinkMocks.CredentialManifestDeepLinkStr.decode())
+        assert(subject.value == DeepLinkMocks.PresentationRequestDeepLinkTestNetStr)
+        assert(subject.value.decode() == DeepLinkMocks.PresentationRequestDeepLinkTestNetStr.decode())
+        assert(subject.requestUri == DeepLinkMocks.PresentationRequestRequestDecodedUriStr)
+        assert(subject.vendorOriginContext == DeepLinkMocks.PresentationRequestVendorOriginContext)
+    }
+
+    @Test
+    fun testPresentationRequestDeepLinkMainNetValidAggregation() {
+
+        subject = VCLDeepLink(value = DeepLinkMocks.PresentationRequestDeepLinkMainNetStr)
+
+        assert(subject.value == DeepLinkMocks.PresentationRequestDeepLinkMainNetStr)
+        assert(subject.value.decode() == DeepLinkMocks.PresentationRequestDeepLinkMainNetStr.decode())
+        assert(subject.requestUri == DeepLinkMocks.PresentationRequestRequestDecodedUriStr)
+        assert(subject.vendorOriginContext == DeepLinkMocks.PresentationRequestVendorOriginContext)
+    }
+
+    @Test
+    fun testCredentialManifestDeepLinkDevNetValidAggregation() {
+
+        subject = VCLDeepLink(value = DeepLinkMocks.CredentialManifestDeepLinkDevNetStr)
+
+        assert(subject.value == DeepLinkMocks.CredentialManifestDeepLinkDevNetStr)
+        assert(subject.value.decode() == DeepLinkMocks.CredentialManifestDeepLinkDevNetStr.decode())
+        assert(subject.requestUri == DeepLinkMocks.CredentialManifestRequestDecodedUriStr)
+        assert(subject.vendorOriginContext == null)
+    }
+
+    @Test
+    fun testCredentialManifestDeepLinkTestNetValidAggregation() {
+
+        subject = VCLDeepLink(value = DeepLinkMocks.CredentialManifestDeepLinkTestNetStr)
+
+        assert(subject.value == DeepLinkMocks.CredentialManifestDeepLinkTestNetStr)
+        assert(subject.value.decode() == DeepLinkMocks.CredentialManifestDeepLinkTestNetStr.decode())
+        assert(subject.requestUri == DeepLinkMocks.CredentialManifestRequestDecodedUriStr)
+        assert(subject.vendorOriginContext == null)
+    }
+
+    @Test
+    fun testCredentialManifestDeepLinkMainNetValidAggregation() {
+
+        subject = VCLDeepLink(value = DeepLinkMocks.CredentialManifestDeepLinkMainNetStr)
+
+        assert(subject.value == DeepLinkMocks.CredentialManifestDeepLinkMainNetStr)
+        assert(subject.value.decode() == DeepLinkMocks.CredentialManifestDeepLinkMainNetStr.decode())
         assert(subject.requestUri == DeepLinkMocks.CredentialManifestRequestDecodedUriStr)
         assert(subject.vendorOriginContext == null)
     }
