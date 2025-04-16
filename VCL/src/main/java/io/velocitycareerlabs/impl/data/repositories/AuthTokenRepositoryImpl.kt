b9fac04cd27653abcc4103d08c69b5b9c032cf92
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-04-16 15:40:02 +0300

    Update AuthTokenRepositoryImpl.kt

--- VCL/src/main/java/io/velocitycareerlabs/impl/data/repositories/AuthTokenRepositoryImpl.kt
+++ VCL/src/main/java/io/velocitycareerlabs/impl/data/repositories/AuthTokenRepositoryImpl.kt
@@ -50,7 +50,7 @@ internal class AuthTokenRepositoryImpl(
                             } ?: run {
                                 completionBlock(
                                     VCLResult.Failure(
-                                        VCLError("Failed to parse: ${authTokenResponse.payload}")
+                                        VCLError("Failed to parse: auth token response")
                                     )
                                 )
                             }
